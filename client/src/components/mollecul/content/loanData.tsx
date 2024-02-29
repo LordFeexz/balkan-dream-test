be24fa081f2f25eca261a330afc420ba20886f61
anandafiqri <anandafiqri1403@gmail.com>
2024-02-29 08:58:13 +0700

    ref:change calculation

--- client/src/components/mollecul/content/loanData.tsx
+++ client/src/components/mollecul/content/loanData.tsx
@@ -15,7 +15,7 @@ export default function LoanData({ data }: LoanDataProps) {
         {data.amount -
           (data?.paymentHistory.length
             ? data.installment * data.paymentHistory.length
-            : data.amount)}
+            : 0)}
       </td>
       <td> {data.note?.description || "N/A"}</td>
     </tr>
